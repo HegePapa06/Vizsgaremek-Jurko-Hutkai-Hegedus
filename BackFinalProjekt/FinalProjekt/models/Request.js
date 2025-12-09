1f5e4b4a264caf6afff8f72199112c679e3325f3
HegePapa06 <160709970+HegePapa06@users.noreply.github.com>
2025-12-09 20:57:05 +0100

    Add files via upload

---
+++ BackFinalProjekt/FinalProjekt/models/Request.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+const User = require('./user');
+
+const Request = sequelize.define("Request", {
+  studentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  teacherId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: "pending"
+  }
+});
+
+module.exports = Request;
+
+
