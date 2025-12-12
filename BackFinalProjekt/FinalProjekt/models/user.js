d1b08bd3b4290ef648318a55a548a3bda2b3f253
HegePapa06 <160709970+HegePapa06@users.noreply.github.com>
2025-12-12 15:30:51 +0100

    Add files via upload

---
+++ BackFinalProjekt/FinalProjekt/models/user.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+// db.js importálása
+const sequelize = require("../db"); 
+
+const User = sequelize.define("user", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    role: {
+        type: DataTypes.ENUM("tanulo", "tanar"),
+        allowNull: false
+    }, 
+    county: { 
+        type: DataTypes.STRING 
+    },
+    bio: { 
+        type: DataTypes.TEXT 
+    },
+    phone: {
+         type: DataTypes.STRING 
+    },
+    photo: { 
+        type: DataTypes.STRING 
+    },
+}, {
+    tableName: "users",
+    timestamps: true
+});
+
+module.exports = User;
