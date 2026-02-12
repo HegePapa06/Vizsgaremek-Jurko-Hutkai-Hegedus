07f1bf0e257b5711dacf7bf0feff0663728d301a
HegePapa06 <160709970+HegePapa06@users.noreply.github.com>
2026-02-12 16:02:00 +0100

    Add files via upload

---
+++ Vizsgaremek-Jurko-Hutkai-Hegedus/Backend/models/user.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db"); 
+
+const User = sequelize.define("user", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    role: {
+        type: DataTypes.ENUM("tanulo", "tanar"),
+        allowNull: false
+    }, 
+    county: { 
+        type: DataTypes.STRING 
+    },
+    bio: { 
+        type: DataTypes.TEXT 
+    },
+    phone: {
+         type: DataTypes.STRING 
+    },
+    photo: { 
+        type: DataTypes.STRING 
+    },
+    canRetakeExam: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    lastExamFailed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    purchasedHours: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+}, {
+    tableName: "users",
+    timestamps: true
+});
+
+module.exports = User;
